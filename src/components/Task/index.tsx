7f26b89cbd5d93fa6f34fea70c5459f01a2d98c6
arielfprado <arielfprado96@gmail.com>
2022-10-17 11:32:05 -0300

    to-do list first commit

---
+++ src/components/Task/index.tsx
@@ -0,0 +1,30 @@
+import { Trash } from "react-feather"
+
+import { IComponentTaskProps } from "./types"
+
+import styles from "./styles.module.css"
+
+export function Task({ task, handleToggleTask, handleRemoveTask }: IComponentTaskProps) {
+  return (
+    <div className={styles.container}>
+      <input
+        type="checkbox"
+        className={styles.input}
+        readOnly
+        checked={task.isCompleted}
+        onClick={() => handleToggleTask(task)}
+      />
+      <label className={task.isCompleted ? styles.completed : ""}>{task.title}</label>
+
+      <div className={styles.buttonsWrapper}>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={() => handleRemoveTask(task.id)}
+        >
+          <Trash size={16} />
+        </button>
+      </div>
+    </div>
+  )
+}
